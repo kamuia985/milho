9d5827b1d78298a4ef5eb1e8e86775e27a105c9c
pedrin <pedroguk3@gmail.com>
2023-09-19 09:00:11 -0300

    puts

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Parse from "parse/react-native.js";
+	import AsyncStorage from '@react-native-async-storage/async-storage';
+
+	Parse.setAsyncStorage(AsyncStorage);
+  Parse.initialize('HppY6kDFUA6aNLDBCwlc2EY4bpABYRYSOUSMHKqB','GAk0vPRgV9s2uSVHyesJUoteLymeW8KDpGRHdYmR');
+	Parse.serverURL = 'https://parseapi.back4app.com/';
 
 export default function App() {
   return (
